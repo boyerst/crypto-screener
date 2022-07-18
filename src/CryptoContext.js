7ae80c6cac3e1d386f05cf5fb9c0395f603be3be
Steve Boyer <stephen.a.boyer@gmail.com>
2022-07-18 14:17:54 -0500

    Add file for createContext API and wrap app with Provider

---
+++ src/CryptoContext.js
@@ -0,0 +1,17 @@
+import Tract from 'react';
+import { createContext } from 'react';
+
+
+const Crypto = createContext()
+
+
+const CryptoContext = ({children}) => {
+  return (
+    <Crypto.Provider>
+      {children}
+    </Crypto.Provider>
+  )
+}
+
+
+export default CryptoContext
