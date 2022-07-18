c955dc983f7588df32e5ff3ce40acc3b78d7db07
Steve Boyer <stephen.a.boyer@gmail.com>
2022-07-18 14:50:04 -0500

    Pass createContext() state to the rest of the app

--- src/CryptoContext.js
+++ src/CryptoContext.js
@@ -10,14 +10,16 @@ const CryptoContext = ({children}) => {
   const [currency, setCurrency] = useState("INR");
   const [symbol, setSymbol] = useState("₹")
 
+  // This will run everytime the currency changes, hence why we have currency set as a dependency
   useEffect(() => {
     if (currency === "INR") setSymbol("₹");
     else if (currency === "USD") setSymbol("$");
+  // This function is dependent on currency, thus whenever currency changes, the function will run and conditionally apply symbols
   }, [currency]);
 
   return (
 
-    <Crypto.Provider>
+    <Crypto.Provider value={{currency, symbol, setCurrency}}>
       {children}
     </Crypto.Provider>
   )
