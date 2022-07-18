64d3566224056eb91fff9dc3b1d14b0832695ea5
Steve Boyer <stephen.a.boyer@gmail.com>
2022-07-18 13:14:06 -0500

    Add dropdown Select element

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,14 +1,23 @@
 import React from 'react';
-import { AppBar, Container, Toolbar, Typography } from "@material-ui/core";
+import { AppBar, Container, Toolbar, Typography, Select, MenuItem  } from "@material-ui/core";
 
 const Header = () => {
   return (
     <AppBar color='transparent' position='static'>
       <Container>
         <Toolbar>
-          <Typography>
-            App Name
-          </Typography>
+          <Typography>App Name</Typography>
+          <Select 
+            variant="outlined"
+            style={{
+              width: 100,
+              height: 40, 
+              marginLeft: 15
+            }}
+            >
+            <MenuItem>USD</MenuItem>
+            <MenuItem>INR</MenuItem>
+          </Select>
         </Toolbar>
       </Container>
     </AppBar>
