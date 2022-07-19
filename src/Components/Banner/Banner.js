a7c63546b09b86a98a2516e0a39ab88ad0aa1337
Steve Boyer <stephen.a.boyer@gmail.com>
2022-07-19 14:07:57 -0500

    Add background image to Banner

--- src/Components/Banner/Banner.js
+++ src/Components/Banner/Banner.js
@@ -1,13 +1,32 @@
-// import React from 'react';
-import "../../App.css";
+import { Container, makeStyles } from "@material-ui/core";
 
+
+
+const useStyles = makeStyles(() => ({
+  banner: {
+    backgroundImage: "url(./banner2.jpeg)",
+  },
+  bannerContent: {
+    height: 400, 
+    display: "flex",
+    flexDirection: "column",
+    paddingTop: 25, 
+    justifyContent: "space-around",
+  },
+}));
 
 
 const Banner = () => {
 
+  const classes = useStyles();
+
   return (
-    <div>
-      Banner
+    <div className={classes.banner}>
+      <Container className={classes.bannerContent}>
+        <div>
+          
+        </div>
+      </Container>
     </div>
   )
 
