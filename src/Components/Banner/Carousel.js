4050a5dedbde4e240781e7164df25cc22c452401
Steve Boyer <stephen.a.boyer@gmail.com>
2022-07-20 14:16:59 -0500

    Add CoinGecko API and Context API to Carousel component

--- src/Components/Banner/Carousel.js
+++ src/Components/Banner/Carousel.js
@@ -1,4 +1,7 @@
 import { makeStyles } from "@material-ui/core";
+import axios from "axios";
+import { TrendingCoins } from "../../config/api";
+import { CryptoState } from "../../CryptoContext";
 
 const useStyles = makeStyles((theme) => ({
   carousel: {
@@ -11,6 +14,14 @@ const useStyles = makeStyles((theme) => ({
 const Carousel = () => {
 
   const classes = useStyles()
+
+  const { currency, setCurrency } = CryptoState()
+
+  const fetchTrendingCoins = async () => {
+    // Pass in the API endpoint
+      // Pass in our currency var into the function
+    const { data } = await axios.get(TrendingCoins(currency))
+  }
 
   return (
     <div className={classes.carousel}>
