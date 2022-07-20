45ce4be1b0dd9dce46be61343d697887145343b0
Steve Boyer <stephen.a.boyer@gmail.com>
2022-07-20 14:30:12 -0500

    Add useEffect for API call and set data from said call into local 'Trending' state

--- src/Components/Banner/Carousel.js
+++ src/Components/Banner/Carousel.js
@@ -1,7 +1,10 @@
 import { makeStyles } from "@material-ui/core";
+import { useState, useEffect } from 'react';
 import axios from "axios";
 import { TrendingCoins } from "../../config/api";
 import { CryptoState } from "../../CryptoContext";
+
+
 
 const useStyles = makeStyles((theme) => ({
   carousel: {
@@ -11,17 +14,30 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
+
 const Carousel = () => {
 
-  const classes = useStyles()
 
-  const { currency, setCurrency } = CryptoState()
+  // Initial state is an empty array
+  const [trending, setTrending] = useState([])
+  const { currency } = CryptoState()
 
   const fetchTrendingCoins = async () => {
     // Pass in the API endpoint
       // Pass in our currency var into the function
+    // We create a new state to manage the { data } we get from the API call
     const { data } = await axios.get(TrendingCoins(currency))
+    // After fetching the data we set it to our 'trending' state
+    setTrending(data)
   }
+
+  // We call fetchTrending when our component is rendered the first time
+  useEffect(() => {
+    fetchTrendingCoins()
+  }, [])
+
+  const classes = useStyles()
+
 
   return (
     <div className={classes.carousel}>
@@ -30,6 +46,7 @@ const Carousel = () => {
   )
 
 }
+
 
 
 
