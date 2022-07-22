a457ca42bf53935f03f7eec384af457d7cbac5d7
Steve Boyer <stephen.a.boyer@gmail.com>
2022-07-22 14:21:01 -0500

    Add API fetch call and state to CoinsTable

--- src/Components/CoinsTable.js
+++ src/Components/CoinsTable.js
@@ -1,6 +1,19 @@
 
 const CoinsTable = () => {
 
+  // Initial state is an empty array
+  const [coins, setCoins] = useState([])
+  const [loading, setLoading] = useState(false)
+
+
+
+
+  const fetchCoinList = async () => {
+
+    const { data } = await axios.get(CoinList(currency))
+
+  }
+
   return (
     <div>
       CoinsTable
