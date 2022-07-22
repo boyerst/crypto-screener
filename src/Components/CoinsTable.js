9721f3afeeb4972d3faf8e9aa6d9ca17b064b5cc
Steve Boyer <stephen.a.boyer@gmail.com>
2022-07-22 14:43:12 -0500

    Add notes on Object Destructuring

--- src/Components/CoinsTable.js
+++ src/Components/CoinsTable.js
@@ -15,6 +15,19 @@ const CoinsTable = () => {
 
 
   const fetchCoins = async () => {
+    // Object Destructuring...
+    // We receive an object from CoinGecko API call called 'data' f - wrapping it in braces destructures the data
+      // If we just declared the data without braces then we would have to refer to said data as 'data.data'
+
+      // const data = {myObject}
+        // const prop = data.myObject.prop
+      // const data = {data}
+        // const prop = data.data.prop
+          // Accessing props requires drilling using chaining (dot notation): data.data.prop
+      // const { data } = {myObject}
+        // const prop = data.prop
+      // const { data } = { data }
+          // Accessing props only requires chaining data directly to prop: data.prop
     const { data } = await axios.get(CoinList(currency))
   }
 
