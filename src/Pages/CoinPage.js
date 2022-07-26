f43e4e3eb99737ff22741bb7ccd86cfa83dca85e
Steve Boyer <stephen.a.boyer@gmail.com>
2022-07-26 11:20:36 -0500

    Import and setup useParams and useState for CoinPage pages

--- src/Pages/CoinPage.js
+++ src/Pages/CoinPage.js
@@ -1,9 +1,18 @@
 import React from 'react';
-import "../App.css";
+import { useParams } from "react-router-dom";
+import { useState } from 'react';
 
 
 
 const CoinPage = () => {
+  // We take the id from our URL that we click on in the HomePage and use this id to fetch one single coin from the CoinGecko API
+    // We do this using useParams() from react-router-dom
+  // We destructure the id var we get from the url
+  const { id } = useParams()
+  // Next we create a 'coin' state to store what we receive from our API
+  const [coin, setCoin] = useState()
+
+
 
   return (
     <div>
