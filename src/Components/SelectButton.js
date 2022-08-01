7494105269e51a0de6c5f799dc37527ffba3223b
Steve Boyer <stephen.a.boyer@gmail.com>
2022-08-01 11:09:59 -0500

    Import and setup makeStyles() for SelectButton

--- src/Components/SelectButton.js
+++ src/Components/SelectButton.js
@@ -1,9 +1,20 @@
+import { makeStyles  } from "@material-ui/core";
+
+
+const useStyles = makeStyles(() => ({
+
+}));
+
+
 
 // Pass in children so that anything written inside of children will be written in the object in the component
 const SelectButton = ({children}) => {
   
+  const classes = useStyles()
+
+
   return(
-    <span>
+    <span className={classes.selectbutton}>
       {children}
     </span>
   )
