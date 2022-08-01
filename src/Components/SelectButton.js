3a610b9138dbafedc99d10465a622f8589058df1
Steve Boyer <stephen.a.boyer@gmail.com>
2022-08-01 11:23:13 -0500

    Add styles to SelectButton

--- src/Components/SelectButton.js
+++ src/Components/SelectButton.js
@@ -1,10 +1,27 @@
 import { makeStyles  } from "@material-ui/core";
 
 
-const useStyles = makeStyles(() => ({
 
+const useStyles = makeStyles((selected, onClick) => ({
+  selectbutton: {
+    border: "1px solid gold",
+    borderRadius: 5,
+    padding: 10,
+    paddingLeft: 20,
+    paddingRight: 20,
+    fontFamily: "Montserrat",
+    cursor: "pointer",
+    backgroundColor: selected ? "gold" : "",
+    color: selected ? "black" : "",
+    fontWeight: selected ? 700 : 500,
+    "&:hover": {
+      backgroundColor: "gold",
+      color: "black",
+    },
+    width: "22%",
+    //   margin: 5,
+  },
 }));
-
 
 
 // Pass in children so that anything written inside of children will be written in the object in the component
@@ -15,7 +32,7 @@ const SelectButton = ({children, selected, onClick}) => {
 
 
   return(
-    <span onclick={onClick} className={classes.selectbutton}>
+    <span onClick={onClick} className={classes.selectbutton}>
       {children}
     </span>
   )
